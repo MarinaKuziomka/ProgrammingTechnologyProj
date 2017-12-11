335fe9d01a55918bf8d2fa08eef33d5b90000195
MarinaKuziomka <marynakuziomka@ukr.net>
2017-12-11 14:17:41 +0200

    Update StatusBuilder.java

--- src/main/java/com/entities/builder/StatusBuilder.java
+++ src/main/java/com/entities/builder/StatusBuilder.java
@@ -3,14 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.wookie.devteam.entities.builder;
+package com.entities.builder;
 
 import com.wookie.devteam.entities.Status;
 
-/**
- *
- * @author wookie
- */
 public class StatusBuilder {
     private Status instance;
 
