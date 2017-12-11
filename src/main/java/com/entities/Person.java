da22ee34f031cff423b2485e6341148f0f53652c
MarinaKuziomka <marynakuziomka@ukr.net>
2017-12-11 23:58:41 +0200

    Add entities

--- src/main/java/com/entities/Person.java
+++ src/main/java/com/entities/Person.java
@@ -2,11 +2,12 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */ 
+ */
 package com.entities;
 
 import java.sql.Date;
 import java.util.Objects;
+
 
 public class Person {
     private Integer id;
